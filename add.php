fa749b193dc7e0e9e2cafce5947d58c4b8bc70d8
Tostes, Rogerio <RTostes@wabtec.com>
2019-05-23 14:57:52 -0300

    Melhorias

--- add.php
+++ add.php
@@ -1,14 +1,23 @@
 <?php
+ 
    	include("connect.php");
    	
-   	$link=Connection();
+	   $link=Connection();
 
-$MSG_Texto=$_POST["MSG_Texto1"];
+$MSG_Texto1=$_POST["MSG_Texto1"];
+$MSG_Texto2=$_POST["MSG_Texto2"];
+$MSG_Texto3=$_POST["MSG_Texto3"];
+$MSG_Texto4=$_POST["MSG_Texto4"];
 
 
-$query=mysqli_query($link,"INSERT INTO `tempLog` (`timeStamp`,`tension`) 
-	VALUES (current_timestamp,'".$MSG_Texto."')"); 
+
+
+$query=mysqli_query($link,"INSERT INTO `tempLog` (`timeStamp`,`tensionNormal`,`tensionReversa`,`tensionLinha1`,`tensionLinha2`) 
+	VALUES (current_timestamp,'".$MSG_Texto1."','".$MSG_Texto2."','".$MSG_Texto3."','".$MSG_Texto4."')"); 
+
   
-     	mysqli_close($link);
+		 mysqli_close($link);
+		 
    		header("Location: tables.php");
 ?>
+
